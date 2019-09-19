0777bc7a821077b86443904bfff8d3d8aea2d727
Adam Stankiewicz <sheerun@sher.pl>
2019-09-20 00:04:57 +0200

    Add initial implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-function gitCommitId () {
-  return true
+const fs = require('fs')
+const path = require('path')
+
+function gitCommitId (options = {}) {
+  const cwd = options.cwd || process.cwd()
+
+  if (!fs.existsSync(path.join(cwd, '.git'))) {
+    return
+  }
+
+  const head = fs.readFileSync(path.join(cwd, '.git', 'HEAD'), 'utf-8')
+  const ref = head.match('refs/heads/.*')[0]
+
+  return fs.readFileSync(path.join(cwd, '.git', ref), 'utf-8').trim()
 }
 
 module.exports = gitCommitId
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,12 @@
+const execSync = require('child_process').execSync
 const gitCommitId = require('../')
 
-it('works', () => {
-  expect(gitCommitId()).toBe(true)
+it('gets git commit id', () => {
+  const commitId = execSync('git rev-parse HEAD').toString().trim()
+  expect(gitCommitId()).toEqual(commitId)
+})
+
+it('return undefined if not in git repository', () => {
+  const commitId = execSync('git rev-parse HEAD').toString().trim()
+  expect(gitCommitId({ cwd: '/tmp' })).toEqual(undefined)
 })
