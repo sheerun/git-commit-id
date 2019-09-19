aef7bc1a957af7e4816f332e58055d69492ac54d
Adam Stankiewicz <sheerun@sher.pl>
2019-09-20 00:42:25 +0200

    Add extra test

--- test/index.test.js
+++ test/index.test.js
@@ -7,8 +7,8 @@ beforeEach(() => {
   execSync(`rm -rf ${cwd} && mkdir ${cwd}`)
 })
 
-function git(commands) {
-  return execSync('git ' + commands, { cwd }).toString().trim()
+function git(commands, options = {}) {
+  return execSync('git ' + commands, { cwd: options.cwd || cwd }).toString().trim()
 }
 
 it('return undefined if not in git repository', () => {
@@ -20,8 +20,13 @@ it('returns emptry tree id for fresh repository', () => {
   expect(gitCommitId({ cwd })).toEqual('4b825dc642cb6eb9a060e54bf8d69288fbee4904')
 })
 
-it('gets git commit id', () => {
+it('gets git commit id for commit', () => {
   git('init')
   git('commit --allow-empty -m test')
   expect(gitCommitId({ cwd })).toEqual(git('rev-parse HEAD'))
+})
+
+it('gets git commit id for this repo', () => {
+  const commitId = git('rev-parse HEAD', { cwd: process.cwd() })
+  expect(gitCommitId()).toEqual(commitId)
 })
